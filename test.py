408f95b10f4144904e14963b17ed6dce8c9742ed
Hélène Tindon <helene.tindon@gmail.com>
2022-12-27 00:38:30 +0100

    Implementing tests for checkForDuplicates

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from just_a_deck import checkForDuplicates
+from just_a_deck import checkForDuplicates, checkForLength
 
 class TestCheckForDuplicates(unittest.TestCase):
     def test_checkForDuplicates(self):
@@ -13,6 +13,23 @@ class TestCheckForDuplicates(unittest.TestCase):
         self.assertTrue(checkForDuplicates(["mi", "ia", "ou"]))
         self.assertTrue(checkForDuplicates(['A', 'B']))
         self.assertTrue(checkForDuplicates([1, 2, 3]))
+
+class TestCheckForLength(unittest.TestCase):
+    def test_checkForLength(self):
+        '''
+        Test that this function can invalidate list that have a length not equal to 30.
+        '''
+        data = list(range(30))
+        res = checkForLength(data)
+        self.assertTrue(res)
+        
+        data = list(range(29))
+        res = checkForLength(data)
+        self.assertFalse(res)
+        
+        data = list(range(31))
+        res = checkForLength(data)
+        self.assertFalse(res)
 
 if __name__ == "__main__":
     unittest.main()
