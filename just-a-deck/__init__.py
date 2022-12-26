4bea786a4f8d04cc0aa4a9c0fe83a51ae2591c3b
Hélène Tindon <helene.tindon@gmail.com>
2022-12-27 00:36:28 +0100

    implementing checkForLength function

--- just-a-deck/__init__.py
+++ just-a-deck/__init__.py
@@ -10,3 +10,16 @@ def checkForDuplicates(listToTest):
     if len(listToTest) != len(set(listToTest)):
         return False
     return True
+
+def checkForLength(item_list):
+    '''
+    This function tests for the length of a given list of items.
+    Only lists containing 30 items are considered valid.
+    >>> checkForLength(list(range(30)))
+    True
+    >>> checkForLength(list(range(29)))
+    False
+    '''
+    if len(item_list) != 30:
+        return False
+    return True
