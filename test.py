2dea514ad3b408b7f30b8586b576eb4d9e47919e
Hélène Tindon <helene.tindon@gmail.com>
2022-12-27 01:54:10 +0100

    Adding test function for validateList

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import unittest
+import random
+import string
 
-from just_a_deck import checkForDuplicates, checkForLength
+from just_a_deck import checkForDuplicates, checkForLength, validateList
 
 class TestCheckForDuplicates(unittest.TestCase):
     def test_checkForDuplicates(self):
@@ -30,6 +32,30 @@ class TestCheckForLength(unittest.TestCase):
         data = list(range(31))
         res = checkForLength(data)
         self.assertFalse(res)
+
+class TestValidateList(unittest.TestCase):
+    def test_validateList(self):
+        '''
+        Test that this function can invalidate list that have either:
+        - a length not equal to 30
+        - at least one duplicate item
+        '''
+        data = list(range(30))
+        res = validateList(data)
+        self.assertTrue(res)
+        
+        data = string.ascii_letters
+        res = validateList(data)
+        self.assertFalse(res)
+        
+        data = list(string.ascii_letters)
+        del data[30:]
+        res = validateList(data)
+        self.assertTrue(res)
+        
+        data = list(string.ascii_letters)
+        while len(data) > 30:
+            data.pop()
 
 if __name__ == "__main__":
     unittest.main()
